82c4190d8bc9943ca79706f991a0b9cd874398d8
AAminha <dks4857@gmail.com>
2022-02-22 19:44:39 +0900

    txt파일 정리 및 ReaderTXT 파일 수정

--- Demo/DB/ReaderTXT.java
+++ Demo/DB/ReaderTXT.java
@@ -12,27 +12,23 @@ public class ReaderTXT {
 	
 	public static void main(String[] args) {
 		try{
-			Path path = Paths.get("txt 파일 절대 경로 입력하기");
+			Path path = Paths.get("txt파일의 절대경로 입력하기");		// 폴더 구분은 '\\'로!!
 			Stream<String> lines = Files.lines(path);
 			String str;
 			
 			String content = lines.collect(Collectors.joining(System.lineSeparator()));
 			String[] Arr = content.split("\t");
 			
-			for (int i = 6; i < Arr.length;) {
+			for (int i = 7; i < Arr.length;i++) {
 				i++;
 				str = Arr[i++];
-				str = str.replaceAll("\"", "");
 				System.out.print("이름 : " + str);
 	            System.out.println(" / 카테고리1 : " + Arr[i++] + " / 카테고리2 : " + Arr[i++]);
 	            str = Arr[i++];
-				str = str.replaceAll("\"", "");
 	            System.out.println("재료 : " + str);
 	            str = Arr[i++];
-				str = str.replaceAll("\"", "");
 	            System.out.print(str);
 	            System.out.println();
-	            i++;
 	            System.out.println("==================================");
 			}
 			
